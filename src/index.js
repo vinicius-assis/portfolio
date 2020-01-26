d469ef225ae8bfad0d118f17726f70cfcf86a8d7
Vinicius Assis <viniciusassis.dev@hotmail.com>
2020-01-26 20:37:34 -0300

    Create object label and size.css to set size variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './settings/colors.css'
+import './style/settings/size.css'
+import './style/settings/colors.css'
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
--- src/objects/Label/index.js
+++ src/objects/Label/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const Label = styled.label`
-  font-size: 1.4em;
+  font-size: var(--size-small);
   font-weight: bold;
 `
 
