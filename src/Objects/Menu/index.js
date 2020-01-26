adccc32b034d4b6305aece358b2892cc22e2a3bd
Vinicius Assis <viniciusassis.dev@hotmail.com>
2020-01-26 16:41:34 -0300

    Create object Menu

--- src/Objects/Menu/index.js
+++ src/Objects/Menu/index.js
@@ -1,10 +1,35 @@
 import React from 'react'
-import './style.css'
+import styled from 'styled-components'
+import { render } from '@testing-library/react'
 
-const Menu = () => (
-  <a href="#menu" className="menu">
-    <span className="center">Menu</span>
-  </a>
-)
+const Center = styled.span`
+  content: '';
+  display: block;
+  width: 100%;
+  height: 4px;
+  border-radius: 2px;
+  background-color: #707070;
+  margin-bottom: 5px;
+`
 
-export default Menu
+const Menu = styled.a.attrs({ title: 'Menu' })`
+  display: block;
+  width: 25px;
+  text-indent: -9999px;
+
+  ::before,
+  ::after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 4px;
+    border-radius: 2px;
+    background-color: #707070;
+  }
+
+  ::before {
+    margin-bottom: 5px;
+  }
+`
+
+export { Center, Menu }
