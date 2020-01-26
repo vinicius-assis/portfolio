9fc2c2db87eec08cf7d0e1d4c8658a6c175a11b7
Vinicius Assis <viniciusassis.dev@hotmail.com>
2020-01-26 16:59:55 -0300

    Create object board

---
+++ src/objects/Board/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Board = styled.article`
+  width: 290px;
+  height: 290px;
+  color: var(--color-zero);
+  border-radius: 5px;
+  box-shadow: 0 3px 6px 0 var(--color-light-negative);
+`
+
+export default Board
