4597d3de378706807f935136c251636a2e82c17f
Vinicius Assis <viniciusassis.dev@hotmail.com>
2020-02-01 10:50:51 -0300

    Create components HeaderInternal

---
+++ src/components/HeaderInternal/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const HeaderInternal = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: var(--spacing-medium) var(--spacing-big)
+`
+
+export default HeaderInternal
