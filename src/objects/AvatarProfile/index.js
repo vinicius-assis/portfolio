f3ec8f864415f70936c75a68e829242120d03f21
Vinicius Assis <viniciusassis.dev@hotmail.com>
2020-02-01 10:54:27 -0300

    Create object AvatarProfile

---
+++ src/objects/AvatarProfile/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import ProfileImage from '../../img/profile1.png'
+
+const AvatarProfile = styled.img.attrs({
+  src: ProfileImage,
+  alt: 'Avatar do Vinícius'
+})`
+`
+
+export default AvatarProfile
