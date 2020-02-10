49ea46e29dbe0373a3cfcb523fd031814d5e78cb
Vinicius Assis <viniciusassis.dev@hotmail.com>
2020-02-10 10:04:34 -0300

    Revert "Revert commits"
    This reverts commit f5798f9604be7c73cd4d319b1ef903bcbe3b23b1.

--- src/components/HeaderInternal/index.js
+++ src/components/HeaderInternal/index.js
@@ -4,7 +4,8 @@ const HeaderInternal = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: var(--spacing-medium) var(--spacing-big)
+  margin: 0 auto 85px;
+  padding: 2em 0;
 `
 
 export default HeaderInternal
--- src/objects/AboutLink/index.js
+++ src/objects/AboutLink/index.js
@@ -1,12 +1,17 @@
 import styled from 'styled-components'
 
 const AboutLink = styled.a`
-  font-size: 1.4rem;
+  font-size: var(--size-small);
   font-weight: bold;
   margin-left: auto;
   margin-right: var(--spacing-small);
+  text-transform: uppercase;
   cursor: pointer;
-  ${props => (props.light ? 'color: var(--color-zero)' : '')}
+  ${props => (props.light ? 'color: var(--color-zero);' : '')}
+
+  @media (min-width: 740px) {
+    font-size: var(--size-medium);
+  }
 `
 
 export default AboutLink
