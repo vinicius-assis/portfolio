4b126080c9fce191b995d942dc945e672fb1f446
Vinicius <viniciusassis.dev@hotmail.com>
2020-08-03 12:37:59 -0300

    Updates

---
+++ precache-manifest.8f5d374557394768b8ce679861a44150.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd14cc7e897f1fb14b070ac182870d2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7e364dd7760001417728",
+    "url": "/portfolio/static/css/main.78cd5922.chunk.css"
+  },
+  {
+    "revision": "7256081531dc319e9858",
+    "url": "/portfolio/static/js/2.55ebfecc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.55ebfecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e364dd7760001417728",
+    "url": "/portfolio/static/js/main.c1589de9.chunk.js"
+  },
+  {
+    "revision": "8b1f77bd3084b2269172",
+    "url": "/portfolio/static/js/runtime-main.ffb8274a.js"
+  },
+  {
+    "revision": "0f6d5282a23c0f2a71c5ae29aed0bd54",
+    "url": "/portfolio/static/media/Curriculum.0f6d5282.pdf"
+  }
+]);
